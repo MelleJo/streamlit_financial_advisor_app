ad13687f0483c11cfef6939681a86683ceeb35a2
MelleJo <mellejorna@gmail.com>
2024-08-16 16:11:48 +0200

    test

--- audio_service.py
+++ audio_service.py
@@ -2,20 +2,15 @@ import streamlit as st
 from streamlit_mic_recorder import mic_recorder
 
 class AudioService:
-    def __init__(self):
-        self.audio_recorder = None
-
-    def start_recording(self):
+    def record_audio(self):
         st.subheader("Neem uw adviesnotities op")
-        st.write("Klik op de 'Stop Opname' knop om de opname te stoppen.")
-        self.audio_recorder = mic_recorder(start_prompt="", stop_prompt="", key="recorder")
-        return self.audio_recorder
+        st.write("Klik op de microfoonknop om de opname te starten. Klik nogmaals om te stoppen.")
 
-    def stop_recording(self, audio_recorder):
-        if audio_recorder:
-            audio = audio_recorder
-            if audio:
-                st.audio(audio['bytes'])
-                st.success("Audio succesvol opgenomen!")
-                return audio['bytes']
+        audio = mic_recorder(start_prompt="Start Opname", stop_prompt="Stop Opname", key="recorder")
+
+        if audio:
+            st.audio(audio['bytes'])
+            st.success("Audio succesvol opgenomen!")
+            return audio['bytes']
+        
         return None
